979679295c440f417fbf6260fc41919d422fd0bd
aaditx23 <adit.pranto@gmail.com>
2023-05-25 14:43:32 +0600

    did collectstatic

--- Hello/settings.py
+++ Hello/settings.py
@@ -120,8 +120,10 @@ STATIC_URL = 'static/'
 
 #Added manually
 STATICFILES_DIRS = [
+ 
     BASE_DIR / "static"
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
