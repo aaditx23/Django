7a59c6a7ed7ca6e8621a52eda9de672f16ff71ec
aaditx23 <adit.pranto@gmail.com>
2023-06-05 12:10:42 +0600

    added Data model

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.models import About
+from datetime import datetime
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -11,5 +13,11 @@ def services(request):
     return render(request, 'services.html')
 
 def about(request):
+    if request.method=="POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        abt = About(name = name, email = email, phone = phone, date= datetime.today())
+        abt.save()
     return render(request, 'about.html')
 #https://source.unsplash.com/random/1280x720/?cars
