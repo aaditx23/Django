21b1746b1cd7838a22e6096ef89f0c3075ecb8c0
aaditx23 <adit.pranto@gmail.com>
2023-06-09 16:59:20 +0600

    Added Success Message

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from home.models import About
 from datetime import datetime
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -13,11 +14,14 @@ def services(request):
     return render(request, 'services.html')
 
 def about(request):
+    
     if request.method=="POST":
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         email = request.POST.get('email')
         abt = About(name = name, email = email, phone = phone, date= datetime.today())
         abt.save()
+        messages.success(request, "Your Information is saved")
+    
     return render(request, 'about.html')
 #https://source.unsplash.com/random/1280x720/?cars
