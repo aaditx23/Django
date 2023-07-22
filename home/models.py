3e17d8f4638bac322bb4a9439ffa3bdbc96d046f
aaditx23 <adit.pranto@gmail.com>
2023-07-23 02:48:54 +0600

    Added Mysql Database

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class About(models.Model):
     name = models.CharField(max_length=50)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
     date = models.DateField()
 
